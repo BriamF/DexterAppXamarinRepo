71da419d3d62b6275b56ae59eb61d12cabfce360
BriamF <briam.flores11@gmail.com>
2021-06-13 13:37:33 -0500

    SplashScreen

---
+++ XamarinApp.Android/SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XamarinApp.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash",
+            Label = "SplashActivity", 
+            MainLauncher = true, 
+            NoHistory = true,
+            ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(typeof(MainActivity)); 
+        }
+    }
+}
